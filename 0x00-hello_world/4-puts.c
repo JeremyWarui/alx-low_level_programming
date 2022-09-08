169c99d9265c9eddc04a8d05ac46d4c1db2b62bd
JeremyWarui <jerywarne7@gmail.com>
2022-09-08 06:43:05 -0700

    Hello, puts - print a string of characters

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * print a string of words using puts()
+  *
+  * Return - always 0 (Success)
+  */
+
+int main(void)
+
+{
+	char my_string[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(my_string);
+
+	return 0;
+
+}
