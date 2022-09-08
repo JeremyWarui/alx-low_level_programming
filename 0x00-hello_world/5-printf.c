b51be81a596619008cdc9a5fb578a5c43f17e5c4
JeremyWarui <jerywarne7@gmail.com>
2022-09-08 07:14:12 -0700

    Hello printf - Using prinf to output a string of characters

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Print the exact string given
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+}
