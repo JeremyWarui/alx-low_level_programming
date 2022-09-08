2a27bf2115ac5c96a2698eb10b731d88a63d6dbf
JeremyWarui <jerywarne7@gmail.com>
2022-09-08 12:13:13 -0700

    UNIX - you have to be a genius to see its simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+  * main - Print a string to the standard error (2)
+  *
+  * Return: return 1 the value of standard output
+  */
+
+int main(void)
+{
+
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	/*
+	 * write(STDOUT_FILENO, "hello world", strlen("hello world"));
+	 */
+
+	write(2, str, strlen(str));
+
+	return (1);
+}
