9786822b98af7f31073b1fbc7a8510ae8205e66e
JeremyWarui <jerywarne7@gmail.com>
2022-09-14 04:32:49 -0700

    Debug the check largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -11,4 +11,14 @@
 
 void positive_or_negative(int i);
 
+/**
+  * largest_number - checks which is the largest number
+  * @a: first integer to check
+  * @b: second integer to check
+  * @c: third integer to check
+  * Return: return the largest integer
+  */
+
+int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
