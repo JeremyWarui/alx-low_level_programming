0baddcdcee03234b8c6b077d1abdbdd657f19d03
JeremyWarui <jerywarne7@gmail.com>
2022-09-14 02:46:56 -0700

    Debug task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 
+/**
+  * positive_or_negative - check if integer is positive or negative
+  * @i: integer placeholder
+  * Return: void
+  */
+
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
