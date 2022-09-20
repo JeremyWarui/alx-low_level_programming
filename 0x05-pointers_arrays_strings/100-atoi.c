42ae0717d975a73e48d2adc11d62eae387a30149
JeremyWarui <jerywarne7@gmail.com>
2022-09-20 14:18:21 -0700

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,3 +6,20 @@
   * Return: integer value
   */
 
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	while (*s++)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	}
+
+	return (num * sign);
+}
