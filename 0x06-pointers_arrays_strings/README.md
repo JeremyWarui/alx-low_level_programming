a49d6a443189cc364b2ee48382117f13f10a3f37
JeremyWarui <jerywarne7@gmail.com>
2022-09-21 16:18:35 -0700

    Function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+# C - More pointers, arrays and strings
+---
+* Function that concatenates two strings:
+>>> It loops through the destination then adds the src to dest.
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+  * _putchar - prints the character
+  */
+
+int _putchar(char c);
+
+/**
+  * _strcat - function tht joins two strings
+  */
+char *_strcat(char *dest, char *src);
+
+
+
+#endif
