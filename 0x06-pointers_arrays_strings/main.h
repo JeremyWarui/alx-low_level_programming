9097802fe4d982f580f0b33d6937cdfde3375549
JeremyWarui <jerywarne7@gmail.com>
2022-09-21 18:29:28 -0700

    Function that compares two strings _strcmp()

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -24,5 +24,9 @@ char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
+/**
+  * _strcmp - function that compares two strings
+  */
+int _strcmp(char *s1, char *s2);
 
 #endif
