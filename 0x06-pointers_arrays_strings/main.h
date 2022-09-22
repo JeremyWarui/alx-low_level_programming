cc94a47de999dc54546cacfc2a8025390a42e275
JeremyWarui <jerywarne7@gmail.com>
2022-09-21 19:17:25 -0700

    Reverse the array

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -29,4 +29,11 @@ char *_strncpy(char *dest, char *src, int n);
   */
 int _strcmp(char *s1, char *s2);
 
+/**
+  * reverse_array - reverse an array
+  */
+
+void reverse_array(int *a, int n);
+
+
 #endif
