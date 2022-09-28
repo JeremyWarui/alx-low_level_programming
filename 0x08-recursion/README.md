c732a2f00639c873aab4127c571fcf6bcaaf002b
JeremyWarui <jerywarne7@gmail.com>
2022-09-28 09:45:01 -0700

    Puts recursion function

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,17 @@
+### RECURSION
+---
+Exercises on recursion in C
+
+* Fuction that prints a string
+
+* Function that prints a string in reverse
+
+* Function taht returns the length of a string
+
+* Function that returns the factorial of a given number
+
+* Function that returns the value of `x` raised to power of `y`
+
+* Function that returns the natural square root of a number
+
+* Function that returns 1 if the input integer is a primer number otherwise returns 0
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,47 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+  * _putchar - print character
+  */
+int _putchar(char c);
+
+
+/**
+  * _puts_recursion - Prints a string
+  */
+
+void _puts_recursion(char *s);
+
+/**
+  * _print_rev_recursion - prints string in reverse
+  */
+void _print_rev_recursion(char *s);
+
+/**
+  * _strlen_recursion - prints length of string
+  */
+int _strlen_recursion(char *s);
+
+/**
+  * factorial - returns factorial of a given int
+  */
+int factorial(int n);
+
+/**
+  * _pow_recursion - return x raised to power of y
+  */
+int _pow_recursion(int x, int y);
+
+/**
+  * _sqrt_recursion - return natural square root of number
+  */
+int _sqrt_recursion(int n);
+
+/**
+  * is_prime_number - check if prime number
+  */
+int is_prime_number(int n);
+
+
+#endif
