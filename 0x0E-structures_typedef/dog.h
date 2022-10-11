852786a013f5cf0f7050e949ca6d3593d3fad8ce
JeremyWarui <jerywarne7@gmail.com>
2022-10-11 13:21:44 -0700

    Initialize variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,28 @@ struct dog
 	char *owner;
 };
 
+
+/**
+  * init_dog - initalize the dog structure
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+  * print_dog - prints a stuct dog
+  */
+
+void print_dog(struct dog *d);
+
+/**
+  * new_dog - create a new dog using new structure type
+  */
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+  * free_dog - free memory
+  */
+
+void free_dog(dog_t *d);
+
 #endif
