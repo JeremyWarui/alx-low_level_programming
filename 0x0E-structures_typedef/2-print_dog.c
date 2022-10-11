a38b55406a6b786a80632a013bec8231d579935c
JeremyWarui <jerywarne7@gmail.com>
2022-10-11 13:40:39 -0700

    Print struct members

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a struct dog
+ * @d: struct dog to print
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	/* ternary operator */
+
+	(d->name == NULL) ? printf("Name: (nil)") : printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	(d->owner == NULL) ? printf("Name: (nil)") : printf("Owner: %s\n", d->owner);
+
+}
