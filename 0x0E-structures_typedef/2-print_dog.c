67bdbc1799fd1f4f36364e86df27eac7ca9197ca
JeremyWarui <jerywarne7@gmail.com>
2022-10-11 13:45:19 -0700

    Print struct members

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@ void print_dog(struct dog *d)
 		return;
 	/* ternary operator */
 
-	(d->name == NULL) ? printf("Name: (nil)") : printf("Name: %s\n", d->name);
+	(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
-	(d->owner == NULL) ? printf("Owner: (nil)") : printf("Owner: %s\n", d->owner);
+	(d->owner == NULL) ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 
 }
