5b04bc2ebd6665ca643332bd20ff39a1c4037ecf
JeremyWarui <jerywarne7@gmail.com>
2022-10-13 02:27:12 -0700

    Iterate through array using function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+  * array_iterator - loops thro the array
+  * @array: the array to loop through
+  * @size: size of array
+  * @action: function to call on each ele
+  * Return: void
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (array != NULL && size > 0 && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
