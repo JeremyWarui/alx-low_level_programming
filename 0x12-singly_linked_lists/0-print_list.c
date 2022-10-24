330b9df1783aa213cd1a65267c3dc743320af728
JeremyWarui <jerywarne7@gmail.com>
2022-10-24 02:47:04 -0700

    Update print list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,20 @@
   */
 size_t print_list(const list_t *h)
 {
-	unsigned int num;
+	size_t num;
 
+	num = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
+		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		num++;
 		h = h->next;
 	}
+
 	return (num);
+
 }
