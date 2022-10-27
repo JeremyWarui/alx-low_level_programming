240c8f95acd4040837fdd9bbc5619d9910bb0315
JeremyWarui <jerywarne7@gmail.com>
2022-10-27 08:44:06 -0700

    Update delete function

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -6,47 +6,41 @@
 * @index: position to delete the node
 * Return: -1 if failed or 1 if successful
 */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *prev;
-	unsigned int i;
-	
-	if (*head == NULL)
-		return (-1);
-	
-	if (index != 0)
-	{
-		prev = *head;
-		for (i = 0; i < (index - 1); i++)
-		{
-			if (prev->next != NULL)
-				prev = prev->next;
-			else
-			{
-				free(prev);
-				return (-1);
-			}
-		}
-		temp = *head;
-		for (i = 0; i < (index); i++)
-		{
-			if (temp->next != NULL)
-				temp = temp->next;
-			else
-			{
-				free(temp);
-				return (-1);
-			}
-		}
-		prev->next = temp->next;
-		free(temp);
-		return (1);
-	}
-	else
-	{
-		temp = *head;
-		free(*head);
-		*head = temp->next;
-		return (1);
-	}
+    listint_t *temp, *prev;
+    unsigned int i;
+
+    if (*head == NULL)
+        return (-1);
+
+    if (index != 0)
+    {
+        prev = *head;
+        for (i = 0; i < (index - 1); i++)
+        {
+            if (prev->next != NULL)
+                    prev = prev->next;
+        }
+
+        temp = *head;
+        for (i = 0; i < (index); i++)
+        {
+            if (temp->next != NULL)
+                temp = temp->next;
+        }
+
+        prev->next = temp->next;
+        free(temp);
+        return (1);
+    }
+
+    else
+    {
+        temp = *head;
+        free(*head);
+        *head = temp->next;
+        return (1);
+    }
 }
