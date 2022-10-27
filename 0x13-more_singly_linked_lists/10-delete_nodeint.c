097551038214120f61d4ed731f63a0877df43297
JeremyWarui <jerywarne7@gmail.com>
2022-10-27 14:13:08 -0700

    Update delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 	}
 
-	if ((index != 0 && prev->next == NULL) || prev == NULL)
+	if ((prev->next == NULL) || prev == NULL)
 	{
 		return (-1);
 	}
