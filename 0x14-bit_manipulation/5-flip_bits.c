86ad8c77d5a4ca06fd70b2b00a20a064779d7073
JeremyWarui <jerywarne7@gmail.com>
2022-10-31 13:51:54 -0700

    UPdate flip the bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,14 +11,12 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int diff = 0;
 	unsigned long int difference;
-	
-	difference = n ^ m;
 
+	difference = n ^ m;
 	while (difference > 0)
 	{
 		diff += (difference & 1);
 		difference  >>= 1;
 	}
-
 	return (diff);
 }
