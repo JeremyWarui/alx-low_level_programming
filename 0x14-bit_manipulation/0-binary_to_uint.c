7fd64821f507398233ffc9abf5dcffa5c686ac1d
JeremyWarui <jerywarne7@gmail.com>
2022-10-31 13:02:38 -0700

    Update binary function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,19 +16,12 @@ unsigned int binary_to_uint(const char *b)
 
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	len -= 1;
-
-	i = 0;
-
-	while (b[i] && len >= 0)
+	for (len--, i = 1; len >= 0; len--, i *= 2)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		if (b[len] & 1)
+			sum += i;
 	}
 
 	return (sum);
