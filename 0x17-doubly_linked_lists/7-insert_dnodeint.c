5176411f1a0eecd20c09d39b536f54ab4b3b8a21
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-12-09 14:13:49 +0000

    Insert node at given po at gievn position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at given position
- * @h: pointer to refer
- * @idx: position to insert
- * n: data for the new node
- * Return: pointer to new node or null if unsuccesful
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
  *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
