dd869dc71dd10f88a540eb871d29640a76154a97
JeremyWarui <jerywarne7@gmail.com>
2022-12-11 20:19:15 +0300

    Print nodes data of looped list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,95 @@
+#include "lists.h"
+
+/**
+ * count_nodes_before_loop - counts no of nodes before loop
+ * @head: pointer to head of list
+ * Return: no of nodes before loop
+ */
+
+int count_nodes_before_loop(const listint_t *head)
+{
+	int count = 0;
+	const listint_t *p, *q;
+
+	p = q = head;
+	while (p && q)
+	{
+		p = p->next;
+		q = q->next->next;
+		count++;
+		if (p == q)
+		{
+			p = head;
+			while (p != q)
+			{
+				p = p->next;
+				q = q->next;
+				count++;
+			}
+			return (count);
+		}
+	}
+	return (0);
+}
+
+/**
+ * isLoop - check if list is loop
+ * @head: pointer to head of the list
+ * Return: 1 if true and 0 if otherwise
+ */
+
+int isLoop(const listint_t *head)
+{
+	const listint_t *p, *q;
+
+	p = q = head;
+	while (p && q)
+	{
+		p = p->next;
+		q = q->next->next;
+		if (p == q)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * print_listint_safe - function to print list
+ * @head: pointer to head of list
+ * Return: no of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int loop;
+	int count = 0;
+	size_t nodes = 0;
+	const listint_t *tmp;
+
+	if (head == NULL)
+		exit(98);
+
+	loop = isLoop(head);
+
+	if (loop == 1)
+	{
+		count = count_nodes_before_loop(head);
+		for (loop = 0; loop < count; loop++)
+		{
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			nodes += 1;
+			tmp = tmp->next;
+		}
+	}
+	else if (loop == 0)
+	{
+		tmp = head;
+		while (tmp != NULL)
+		{
+			printf("[%p] %d\n", (void *)tmp, tmp->n);
+			nodes += 1;
+			tmp = tmp->next;
+		}
+	}
+	return (nodes);
+}
